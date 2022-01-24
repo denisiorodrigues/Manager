634a9b3b605cb8e5bf7380c7c000800f5b601f45
= <=>
2022-01-24 08:04:39 -0300

    Add project files.

---
+++ src/Manager.Domain/Entities/User.cs
@@ -0,0 +1,64 @@
+﻿using Manager.Domain.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Entities
+{
+    public class User : Base
+    {
+        public string Name { get; private set; }
+        public string Email { get; private set; }
+        public string Password { get; private set; }
+
+        //EF
+        protected User()
+        {
+            _errors = new List<string>();
+        }
+
+        public User(string name, string email, string password)
+        {
+            Name = name;
+            Email = email;
+            Password = password;
+            _errors = new List<string>();
+        }
+
+        public void ChangeName(string name)
+        {
+            Name = name;
+            Validate();
+        }
+
+        public void ChangePassword(string password)
+        {
+            Password = password;
+            Validate();
+        }
+
+        public void ChangeEmail(string email)
+        {
+            Email = email;
+            Validate();
+        }
+
+        public override bool Validate()
+        {
+            var validator = new UserValidator();
+            var validation = validator.Validate(this);
+
+            if (!validation.IsValid)
+            {
+                foreach (var erro in validation.Errors)
+                {
+                    _errors.Add(erro.ErrorMessage);
+
+                    throw new Exception("Alguns campos estão inválidos. Favor corrigir.");
+                }
+            }
+        }
+    }
+}
---
+++ src/Manager.Domain/Validators/UserValidator.cs
@@ -0,0 +1,69 @@
+﻿using FluentValidation;
+using Manager.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager.Domain.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x)
+                .NotEmpty()
+                .WithMessage("A entidade não pode ser vazia")
+                
+                .NotNull()
+                .WithMessage("A entidde não pode ser nula");
+
+            RuleFor(x => x.Name)
+                .NotNull()
+                .WithMessage("O nome não pode ser nulo")
+                
+                .NotEmpty()
+                .WithMessage("O Nome não pode ser vazio")
+                
+                .MinimumLength(3)
+                .WithMessage("O nome deve ter no mínimo 3 caracteres")
+
+                .MaximumLength(80)
+                .WithMessage("O nome deve ter no máximo 80 caracteres");
+
+            RuleFor(x => x.Email)
+                .NotNull()
+                .WithMessage("O e-mail não pode ser nulo")
+
+                .NotEmpty()
+                .WithMessage("O e-mail não pode ser vazio")
+
+                .MinimumLength(10)
+                .WithMessage("O e-mail deve ter no mínimo 10 caracteres")
+
+                .MaximumLength(180)
+                .WithMessage("O e-mail deve ter no máximo 180 caracteres")
+                
+                .Matches("^[A-Z0-9._%+-]{1,64}@(?:[A-Z0-9-]{1,63}\\.){1,125}[A-Z]{2,63}$")
+                .WithMessage("E-mail inválido pelo regex")
+                
+                .EmailAddress()
+                .WithMessage("E-amil inválido pelo flunet validation");
+
+            RuleFor(x => x.Password)
+               .NotNull()
+               .WithMessage("A senha não pode ser nula")
+
+               .NotEmpty()
+               .WithMessage("A senha não pode ser vazia")
+
+               .MinimumLength(3)
+               .WithMessage("A senha  deve ter no mínimo 3 caracteres")
+
+               .MaximumLength(15)
+               .WithMessage("A senha deve ter no máximo 80 caracteres");
+
+        }
+    }
+}
